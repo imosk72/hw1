b6b6b94d62767fd5cf6b7cef9781ee69fb4f159c
imosk72 <imoskovchenko72@gmail.com>
2022-11-09 19:41:05 +0500

    solution

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+
+Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << 4);
+var stopWatch = new Stopwatch();
+
+var thread1 = new Thread(() =>
+{
+    stopWatch.Start();
+    while (stopWatch.IsRunning)
+    {
+    }
+})
+{
+    Priority = ThreadPriority.Highest
+};
+var thread2 = new Thread(() =>
+{
+    while (true)
+    {
+        if (!stopWatch.IsRunning) continue;
+        stopWatch.Stop();
+        Console.WriteLine($"Tread1 was working {stopWatch.Elapsed.Milliseconds} milliseconds");
+        return;
+    }
+})
+{
+    Priority = ThreadPriority.Highest
+};
+thread2.Start();
+thread1.Start();
+thread2.Join();
+thread1.Join();
