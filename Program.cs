70a92b65ce7c08279d87f135c997b08ba22af160
imosk72 <imoskovchenko72@gmail.com>
2022-11-10 11:23:19 +0500

    explanation

--- Program.cs
+++ Program.cs
@@ -1,33 +1,34 @@
 ﻿using System.Diagnostics;
 
-
+// привязываем к последнему ядру 
 Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << 4);
 var stopWatch = new Stopwatch();
 
 var thread1 = new Thread(() =>
 {
     stopWatch.Start();
-    while (stopWatch.IsRunning)
-    {
-    }
-})
-{
-    Priority = ThreadPriority.Highest
-};
+    while (stopWatch.IsRunning) { }
+}) { Priority = ThreadPriority.Highest };
+
 var thread2 = new Thread(() =>
 {
     while (true)
     {
         if (!stopWatch.IsRunning) continue;
         stopWatch.Stop();
-        Console.WriteLine($"Tread1 was working {stopWatch.Elapsed.Milliseconds} milliseconds");
+        var milliseconds = stopWatch.Elapsed.Milliseconds;
+        Console.WriteLine($"Tread1 was working {milliseconds} milliseconds");
         return;
     }
-})
-{
-    Priority = ThreadPriority.Highest
-};
+}) { Priority = ThreadPriority.Highest };
+
 thread2.Start();
+//запускаем поток, который запускает таймер
 thread1.Start();
+//ждем окончания работы второго потока, когда отключится таймер
+//первый поток точно будет работать свой один квант времени,
+//потом в следующий квант запустится следующий поток, который отключит таймер и скажет время работы первого
 thread2.Join();
 thread1.Join();
+
+//итого получилось: Tread1 was working 31 milliseconds
