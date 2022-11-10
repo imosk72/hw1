64b031f6d03addf4fb3406048d89e1319e279820
imosk72 <imoskovchenko72@gmail.com>
2022-11-10 16:37:58 +0500

    median value

--- Program.cs
+++ Program.cs
@@ -1,34 +1,41 @@
 ﻿using System.Diagnostics;
 
+const int measurementsCount = 11;
 // привязываем к последнему ядру 
 Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << 4);
-var stopWatch = new Stopwatch();
+var measurementResults = new List<int>();
 
-var thread1 = new Thread(() =>
-{
-    stopWatch.Start();
-    while (stopWatch.IsRunning) { }
-}) { Priority = ThreadPriority.Highest };
 
-var thread2 = new Thread(() =>
+for (var _ = 0; _ < measurementsCount; _++)
 {
-    while (true)
+    var stopWatch = new Stopwatch();
+    var thread1 = new Thread(() =>
     {
-        if (!stopWatch.IsRunning) continue;
-        stopWatch.Stop();
-        var milliseconds = stopWatch.Elapsed.Milliseconds;
-        Console.WriteLine($"Tread1 was working {milliseconds} milliseconds");
-        return;
-    }
-}) { Priority = ThreadPriority.Highest };
+        stopWatch.Start();
+        while (stopWatch.IsRunning) { }
+    }) { Priority = ThreadPriority.Highest };
 
-thread2.Start();
-//запускаем поток, который запускает таймер
-thread1.Start();
-//ждем окончания работы второго потока, когда отключится таймер
-//первый поток точно будет работать свой один квант времени,
-//потом в следующий квант запустится следующий поток, который отключит таймер и скажет время работы первого
-thread2.Join();
-thread1.Join();
+    var thread2 = new Thread(() =>
+    {
+        while (true)
+        {
+            if (!stopWatch.IsRunning) continue;
+            stopWatch.Stop();
+            measurementResults.Add(stopWatch.Elapsed.Milliseconds);
+            return;
+        }
+    }) { Priority = ThreadPriority.Highest };
 
-//итого получилось: Tread1 was working 31 milliseconds
+    thread2.Start();
+    //запускаем поток, который запускает таймер
+    thread1.Start();
+    //ждем окончания работы второго потока, когда отключится таймер
+    //первый поток точно будет работать свой один квант времени,
+    //потом в следующий квант запустится следующий поток, который отключит таймер и скажет время работы первого
+    thread2.Join();
+    thread1.Join();
+}
+measurementResults.Sort();
+Console.WriteLine($"median time: {measurementResults[measurementsCount / 2]} ms");
+
+//итого: median time: 32 ms
